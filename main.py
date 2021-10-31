5c6f0921223052547f61c4667141cf9266cc29ef
Catherine <catherine.wallin@gmail.com>
2021-10-31 16:34:30 -0700

    V1: saving

---
+++ main.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+#import pandas as pd
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--incognito")
+
+s=Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s)
+driver.maximize_window()
+driver.get('https://www.kroger.com/savings/cl/coupons/')
+#driver.find_element(By.NAME, 'q').send_keys('Yasser Khalil')
