4540a268ef02e561be4b0941f4c456ed2fc0761a
Catherine <catherine.wallin@gmail.com>
2021-11-07 07:28:02 -0800

    v17: Up and running with PCC website. need to organize discount text

--- main.py
+++ main.py
@@ -15,46 +15,7 @@ chrome_options.add_argument("--incognito")
 s=Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
 driver.maximize_window()
-'''
-driver.get('https://www.publix.com/savings/digital-coupons')
 
-time.sleep(1)
-
-while True:
-    try:
-        button = driver.find_element(By.XPATH, "//*[contains(text(), 'Load More')]")
-    except NoSuchElementException:
-        break
-    button.click()
-
-source = driver.page_source
-
-soup = bs4.BeautifulSoup(source, 'html.parser')
-
-data = {}
-data['item'] = []
-
-items = soup.find_all(class_="card savings -coupon card-ui-responsive")
-id = 0
-
-for item in items:
-    promotion = item.find("div", class_="card-title")
-    discount = item.find("div", class_="sub-title")
-    description = item.find("span", class_="clamp-2")
-    expiration = item.find("div", class_="validity text-block-default")
-    image = item.find("img")
-    url = item.get('href')
-    data['item'].append({
-        'id': id,
-        'promotion': promotion.text,
-        'discount': discount.text,
-        'description': description.text,
-        'expiration': expiration.text,
-        'src': image['src']
-    })
-
-driver.close()
-'''
 driver.get('https://www.pccmarkets.com/departments/weekly-specials/all/')
 
 time.sleep(1)
