962d3f45e5fdc85f1124b4aceffef4332cd3f33f
Catherine <catherine.wallin@gmail.com>
2021-11-07 16:10:53 -0800

    v19: updated with wrapper function

--- main.py
+++ main.py
@@ -9,67 +9,76 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 import time
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--incognito")
+def get_coupon_data():
 
-s=Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
-driver.maximize_window()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--incognito")
 
-driver.get('https://www.pccmarkets.com/departments/weekly-specials/all/')
+    s=Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
+    driver.maximize_window()
 
-time.sleep(1)
+    driver.get('https://www.pccmarkets.com/departments/weekly-specials/all/')
 
-while True:
-    try:
-        button = driver.find_element(By.CLASS_NAME, "btn pc--weekly-specials-headline")
-    except NoSuchElementException:
-        break
-    button.click()
+    time.sleep(1)
 
-source = driver.page_source
+    while True:
+        try:
+            button = driver.find_element(By.CLASS_NAME, "btn pc--weekly-specials-headline")
+        except NoSuchElementException:
+            break
+        button.click()
 
-soup = bs4.BeautifulSoup(source, 'html.parser')
+    source = driver.page_source
 
-data = {}
-data['item'] = []
+    soup = bs4.BeautifulSoup(source, 'html.parser')
 
-items = soup.find_all(class_="card pcc-weekly-special pcc-weekly-special-featured")
-items += soup.find_all(class_="card pcc-weekly-special")
-id = 0
+    data = {}
+    data['item'] = []
 
-for item in items:
-    product = item.find("h3", class_="h5 pcc-weekly-special-headline")
-    discount_text = item.find_all("p", class_="pcc-weekly-special-price")
-    discount = []
-    for x in discount_text:
-        discount.append(x.text)
-    brand = item.find("div", class_="pcc-weekly-special-label")
-    data['item'].append({
-        'id': id,
-        'product': product.text,
-        'discount': discount,
-        'brand': brand.text,
-    })
-    id += 1
+    items = soup.find_all(class_="card pcc-weekly-special pcc-weekly-special-featured")
+    items += soup.find_all(class_="card pcc-weekly-special")
+    id = 0
 
-driver.close()
+    for item in items:
+        product = item.find("h3", class_="h5 pcc-weekly-special-headline")
+        discount_text = item.find_all("p", class_="pcc-weekly-special-price")
+        discount = []
+        for x in discount_text:
+            discount.append(x.text)
+        brand = item.find("div", class_="pcc-weekly-special-label")
+        data['item'].append({
+            'id': id,
+            'product': product.text,
+            'discount': discount,
+            'brand': brand.text,
+        })
+        id += 1
 
-with open("coupon_data.json", "w") as outfile:
-    json.dump(data, outfile)
+    driver.close()
 
-app = Flask(__name__)
-api = Api(app)
+    with open("coupon_data.json", "w") as outfile:
+        json.dump(data, outfile)
+
+    app = Flask(__name__)
+    api = Api(app)
+
+    class Coupons(Resource):
+        def get(self):
+            return data, 200
+
+    api.add_resource(Coupons, '/coupons')
+
+    if __name__ == '__main__':
+        app.run(debug=True)
+
+    return
+
+get_coupon_data()
 
 
-class Coupons(Resource):
-    def get(self):
-        return data, 200
 
 
-api.add_resource(Coupons, '/coupons')
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
 
